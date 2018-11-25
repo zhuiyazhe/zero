8a2800a3532d098a98cfa67e6e2b8146898bacfc
toguanli <15613968752@163.com>
2018-11-25 21:58:08 +0800

    1.首次提交，包含一些基本的功能。 index.html中ul下li的title属性的值是对应的iframe中要显示的网页的名称（不包含后缀）。 ol中的li由iframe中，id为navi的ol下的li定义。

---
+++ js/main.js
@@ -0,0 +1,67 @@
+
+var indexIFrame;
+var iFrameSrcHtml;
+var indexNavi;
+
+var deleteAllChild = function(ele) {
+    while(ele.childNodes.length>0)
+    {
+    	var node = ele.childNodes[0];
+        ele.removeChild(node);
+    }
+}
+
+var onNaviClick = function() {
+	indexIFrame.src = iFrameSrcHtml+"#"+this.title;
+    console.log(iFrameSrcHtml+"#"+this.title);
+}
+
+var setIndexNavi = function() {
+	var tmp = indexIFrame.contentWindow.document.getElementById("navi");
+    if(null == tmp)
+        return;
+    
+    console.log(tmp);
+    var childElement = tmp.firstElementChild;
+    while(null != childElement)
+    {
+        console.log(childElement);
+        
+        var t = document.createElement("li");
+        t.title = childElement.title;
+        t.innerHTML = childElement.innerHTML;
+        t.onclick=onNaviClick;
+    	indexNavi.appendChild(t);
+        childElement = childElement.nextElementSibling;
+    }
+}
+
+var chooseContent = function() {
+    console.log(this.title+".html");
+    
+    iFrameSrcHtml = "sub/" + this.title + ".html";
+    indexIFrame.src = iFrameSrcHtml;
+   
+    deleteAllChild(indexNavi);
+}
+
+
+var installProcess = function() {
+	var contentType = document.getElementById("content_type");
+    console.log(contentType);
+    var firstChild = contentType.firstElementChild;
+    console.log(firstChild);
+    firstChild.onclick = chooseContent;
+    
+    for(var next = firstChild; next != null; next = next.nextElementSibling)
+    {
+    	next.onclick = chooseContent;
+    }
+    
+    indexIFrame = document.getElementById("content");
+    indexIFrame.onload = setIndexNavi;
+    indexNavi = document.getElementById("indexNavi");
+}
+
+installProcess();
+
